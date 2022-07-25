3ed47597bc266be3c7f96357e645d57b1a25e4f2
zubyCharles <zubecharles@gmail.com>
2022-07-25 15:24:38 +0100

    added error handling

--- src/script.js
+++ src/script.js
@@ -5,13 +5,22 @@ const messageID = document.querySelector('.message-id');
 function getMessage() {
   fetch(`https://api.adviceslip.com/advice`)
     .then((res) => res.json())
-    .then((res) => {
-      console.log(res);
-      return res.slip;
+    .then((json) => {
+      console.log(json);
+      return json.slip;
     })
     .then((slip) => {
       message.textContent = `"${slip.advice}"`;
       messageID.textContent = `ADVICE  #${slip.id}`;
+    })
+    .catch((error) => {
+      message.textContent = `Unable to fetch quote, check internet connection`;
+      setTimeout(
+        () =>
+          (message.textContent = `Click the die for the advice that you seek...`),
+        4000
+      );
+      console.error(error);
     })
     .finally(() => {
       fetchMessage.disabled = true;
